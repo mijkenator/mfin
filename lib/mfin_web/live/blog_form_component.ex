7f4c1af0431c18c8ab21b0236164258d083a15c0
Mikhail Khomenkov <mijkenator@gmail.com>
2025-10-12 15:59:06 -0400

    new blog post modal appear ok

--- lib/mfin_web/live/blog_form_component.ex
+++ lib/mfin_web/live/blog_form_component.ex
@@ -6,6 +6,7 @@ defmodule MfinWeb.BlogFormComponent do
 
   @impl true
   def update(assigns, socket) do
+    IO.puts("update BFC:")
     {:ok, assign_changeset(assigns, socket)}
   end
 
@@ -24,6 +25,8 @@ defmodule MfinWeb.BlogFormComponent do
     post = %Post{
       #author_id: current_user.id,
       author_id: 1,
+      title: "new title",
+      content: "new contrwnt",
       documents: []
     }
     ##changeset = Mfin.Egjob.changeset(%Mfin.Egjob{}, assigns)
@@ -38,19 +41,61 @@ defmodule MfinWeb.BlogFormComponent do
     #      |> Enum.map(fn {k, v} -> {Atom.to_string(k), v} end) 
     #      |> Enum.into(%{}) 
     #end
-    #
-    #IO.puts("AC CHANGESET: #{inspect(changeset)}")
+    changeset = %{ "id" => 0}
+    IO.puts("AC CHANGESET: #{inspect(changeset)}")
+    mform = to_form(Blog.change_post(post))
+    IO.puts("BFC FORM: #{inspect(mform, limit: :infinity)}")
     socket
       #|> assign(:title, assigns[:title])
       #|> assign(:id, assigns[:id] )
-      #|> assign(:changeset, changeset)
+      |> assign(:changeset, changeset)
       |> assign(:post, post)
-      |> assign(:form, to_form(Blog.change_post(post)))
-    socket
+      |> assign(:form, mform)
+      |> assign(:documents, [])
+      |> allow_upload(:document,
+       accept: ~w(.pdf .jpg .png),
+       max_entries: 5,
+       max_file_size: 10_000_000,
+       auto_upload: true,
+       progress: &handle_progress/3
+     )
   end
   
   defp error_to_string(:too_large), do: "File is too large (max 10MB)"
   defp error_to_string(:not_accepted), do: "Unacceptable file type (PDF, JPG, PNG only)"
   defp error_to_string(:too_many_files), do: "Too many files selected"
+
+
+  @impl true
+  def handle_event(event, params, socket) do
+    IO.puts("BFC EVENT:#{inspect(event)}  #{inspect(params)}")
+    {:noreply, socket}
+  end
+  
+  defp handle_progress(:document, entry, socket) do
+    if entry.done? do
+      Logger.debug("Upload finished")
+
+      document =
+        consume_uploaded_entry(socket, entry, fn %{path: path} ->
+          upload = %Plug.Upload{
+            content_type: entry.client_type,
+            filename: entry.client_name,
+            path: path
+          }
+
+          {:ok, document} =
+            Blog.create_unattached_document(%{"file" => upload}, socket.assigns.current_user)
+
+          document
+        end)
+
+      {:noreply,
+       socket
+       |> update(:documents, &(&1 ++ [document]))}
+    else
+      {:noreply, socket}
+    end
+  end
 
 end
