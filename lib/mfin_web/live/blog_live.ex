66caf5baa1bfa47391a416a2a35449a59a35c5e3
Mikhail Khomenkov <mijkenator@gmail.com>
2025-10-25 19:15:33 -0400

    save post

--- lib/mfin_web/live/blog_live.ex
+++ lib/mfin_web/live/blog_live.ex
@@ -22,6 +22,12 @@ defmodule MfinWeb.BlogLive do
   def handle_params(params, url, socket) do
     IO.puts("PARAMS: #{inspect(url)}  -> #{inspect(params)}")
     IO.puts("BLHP: #{inspect(socket.assigns, limit: :infinity, printable_limit: :infinity)}")
+
+    la = case socket.assigns.live_action do
+      nil -> :new
+      live_action -> live_action
+    end
+
     case params["action"] do
       nil ->
         {:noreply,
@@ -30,9 +36,12 @@ defmodule MfinWeb.BlogLive do
           |> assign_blog()
         }
       action ->  
+        post = Blog.get_post!( params["id"])
         socket = socket
         |> assign(:live_action, action)
         |> assign(:post_id, params["id"])
+        |> assign(:post, post)
+        |> assign(:documents, post.documents)
         |> assign(:changeset, Mfin.Egjob.get_job_byid(String.to_integer(params["id"])))
         {:noreply, assign_blog(socket)}
     end
@@ -44,10 +53,48 @@ defmodule MfinWeb.BlogLive do
     |> assign(:blog, Blog.get_all_posts(params))
   end
   
+  @impl true
+  def handle_event("validate", %{"post" => post_params}, socket) do
+    changeset =
+      Blog.change_post(socket.assigns.post, post_params)
+
+    {:noreply,
+     socket
+     |> assign(form: to_form(changeset, action: :validate))}
+  end
+  
+  @impl true
+  def handle_event("save", %{"post" => post_params}, socket) do
+    IO.puts("BLHE: #{inspect(socket.assigns, limit: :infinity)}")
+    IO.puts("BLHE LiveAction: #{inspect(socket.assigns.live_action, limit: :infinity)}")
+    la = case socket.assigns.live_action do
+      nil -> :new
+      live_action -> live_action
+    end
+    save_post(socket, la, post_params)
+  end
+
   def handle_event("toggle-activeness", assigns, socket) do
     IO.puts("POST TOGGLE: #{inspect(assigns)}")
     Blog.toggle_post_status(String.to_integer(assigns["id"]))
     {:noreply, assign_blog(socket)}
+  end
+  
+  defp save_post(socket, "edit", post_params) do
+    case Blog.update_post(
+           socket.assigns.post,
+           post_params,
+           socket.assigns.documents
+         ) do
+      {:ok, post} ->
+        {:noreply,
+         socket
+         |> put_flash(:info, "Post updated successfully")
+         |> push_navigate(to: ~p"/blog")}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign(socket, form: to_form(changeset))}
+    end
   end
   
 end
