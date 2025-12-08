6487a394ba8c1a9461933268ca7e4bfa87c3e615
Mikhail Khomenkov <mijkenator@gmail.com>
2025-12-07 20:37:50 -0500

    clean up

--- lib/mfin_web/live/blog_live.ex
+++ lib/mfin_web/live/blog_live.ex
@@ -17,14 +17,6 @@ defmodule MfinWeb.BlogLive do
             nil
            end
       end)
-       #|> assign(:selected_posts, sp)
-      # |> allow_upload(:document,
-      #   accept: ~w(.pdf .jpg .png),
-      #   max_entries: 5,
-      #   max_file_size: 10_000_000,
-      #   auto_upload: true,
-      #   progress: &handle_progress/3
-      # )
     }
   end
   
@@ -117,31 +109,6 @@ defmodule MfinWeb.BlogLive do
     }
   end
 
-  defp handle_progress(:document, entry, socket) do
-    if entry.done? do
-      Logger.debug("Upload finished")
-
-      document =
-        consume_uploaded_entry(socket, entry, fn %{path: path} ->
-          upload = %Plug.Upload{
-            content_type: entry.client_type,
-            filename: entry.client_name,
-            path: path
-          }
-
-          {:ok, document} =
-            Blog.create_unattached_document(%{"file" => upload}, socket.assigns.current_user)
-
-          document
-        end)
-
-      {:noreply,
-       socket
-       |> update(:documents, &(&1 ++ [document]))}
-    else
-      {:noreply, socket}
-    end
-  end
   
 end
 
